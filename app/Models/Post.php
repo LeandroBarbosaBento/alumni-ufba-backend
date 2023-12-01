d22303ef25f7bfbda8e862ffa4afb5aa581bed8d
leandro <leandro@pop-os.localdomain>
2023-12-01 18:27:59 -0300

    changes on models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Comment;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
